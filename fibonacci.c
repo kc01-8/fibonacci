829f6b246f2b24b9ad2d0e422da488c110fab090
kc01-8 <minecraftandcallofduty101@gmail.com>
2023-08-08 01:10:05 +0300

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,44 +1,41 @@
 #include <stdio.h>
-int i;
-int tnd;
-char tta;
+
+int Fib(int n){
+    if(n == 0){
+        return 0;
+    }
+    else if(n == 1){
+        return 1;
+    }
+    else{
+        return Fib(n-1) + Fib(n-2);
+    }
+}
 
 int main(){
-    printf("Print the fibonacci sequence x times (enter a number):\n");
+    int i;
+    int tnd;
+    char tta = 'n';
+
+    printf("Print the Fibonacci sequence x times (enter a number):\n");
     scanf("%d", &tnd);
     printf("\nYou chose %d times.\n", tnd);
     printf("Print each as a new line? (default in one line, y / n):\n");
-    scanf("%d", &tta);
+    scanf(" %c", &tta);
 
-    if(tta == 'y'){
+    if(tta == 'y' || tta == 'Y'){
         printf("1\n");
         for (i = 2; i < tnd+1; i++){
-            int j = i - 1 + i - 2;
+            int j = Fib(i);
             printf("%d\n", j);
         }
     } else{
         printf("1, ");
         for (i = 2; i < tnd+1; i++){
-            int j = i - 1 + i - 2;
+            int j = Fib(i);
             printf("%d, ", j);
         }
     }
+
+    return 0;
 }
-
-
-/*
-The above program has some extra features
-The program below simply prints the sequence 100 times on a new line
-
-#include <stdio.h>
-int i;
-
-int main(){
-        printf("1\n");
-        for (i = 2; i < 101; i++){
-            int j = i - 1 + i - 2;
-            printf("%d\n", j);
-        }
-}
-
-*/
